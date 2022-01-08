e5f9279127a9ae17c6e1a60733f678bec5d75ab0
Ahmad Alexander <ahmadalexanderr@gmail.com>
2022-01-08 14:58:22 +0700

    Upload Tugas 2 Js

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var num = 30; //number
+var str = "Alexaander" //String
+var bol = true;
+var non = null;
+var arr = [3, 5, 1];
+
+console.log(num);
+console.log(str);
+console.log(bol);
+console.log(non);
+console.log(arr[0]);
